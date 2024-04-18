3c7f4c06bf386f72849ce98ee53d29b641d98fe2
aleksfadini <aleksfadini@gmail.com>
2024-04-18 18:09:04 -0400

    fixed deployment script

--- deploy.sh
+++ deploy.sh
@@ -119,16 +119,16 @@ cd ..
 echo -e "${GREEN}Preparing the API...${NC}"
 cd na-my-file-api
 npm install || { echo -e "${RED}Failed to install dependencies for API.${NC}"; exit 1; }
-npm run generate-oas-route-specs || { echo -e "${RED}Failed to generate OAS route specs.${NC}"; exit 1; }
-npm run generate-oas-full || { echo -e "${RED}Failed to generate OAS.${NC}"; exit 1; }
+AWS_PROFILE="$aws_profile" npm run generate-oas-route-specs || { echo -e "${RED}Failed to generate OAS route specs.${NC}"; exit 1; }
+AWS_PROFILE="$aws_profile" npm run generate-oas-full || { echo -e "${RED}Failed to generate OAS.${NC}"; exit 1; }
 cd ..
 
 # Install the API client dependencies
 echo -e "${GREEN}Installing API client dependencies...${NC}"
 cd na-my-file-api-client
 npm install || { echo -e "${RED}Failed to install dependencies for API client.${NC}"; exit 1; }
-npx --yes @hey-api/openapi-ts -i ../na-my-file-api/custom-swagger.json -o ./src || { echo -e "${RED}Failed to generate the TypeScript code from the Swagger specificationins.${NC}"; exit 1; }
-npm run build || { echo -e "${RED}Failed to build the API client.${NC}"; exit 1; }
+AWS_PROFILE="$aws_profile" npx --yes @hey-api/openapi-ts -i ../na-my-file-api/custom-swagger.json -o ./src || { echo -e "${RED}Failed to generate the TypeScript code from the Swagger specificationins.${NC}"; exit 1; }
+AWS_PROFILE="$aws_profile" npm run build || { echo -e "${RED}Failed to build the API client.${NC}"; exit 1; }
 cd ..
 
 # Deploying the API
